2e003ab0ce6716aed78d1471b7fac3305c4d9508
as3ad01 <saifas3ad01@gmail.com>
2025-07-11 23:24:17 +0300

    .

--- kaari-web/src/pages/help/page.tsx
+++ kaari-web/src/pages/help/page.tsx
@@ -6,8 +6,7 @@ import TextAreaBaseModel from '../../components/skeletons/inputs/input-fields/te
 import SelectFieldBaseModelVariant1 from '../../components/skeletons/inputs/select-fields/select-field-base-model-variant-1';
 import { PurpleButtonLB60 } from '../../components/skeletons/buttons/purple_LB60';
 import supportImage from '../../assets/images/support.svg';
-import ReferralPassGreenSkeleton from '../../components/skeletons/cards/referral-pass-green';
-import ReferralPassRedSkeleton from '../../components/skeletons/cards/referral-pass-red';
+
 
 const HelpPage: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -141,8 +140,7 @@ const HelpPage: React.FC = () => {
               onClick={handleSubmit}
             />
           </div>
-          <ReferralPassGreenSkeleton />
-          <ReferralPassRedSkeleton />
+
         </div>
 
 
