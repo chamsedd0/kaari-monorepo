5225567862da6bd33e79a9bc8d93b272149d1de2
as3ad01 <saifas3ad01@gmail.com>
2025-07-20 00:26:45 +0300

    .

--- kaari-web/src/components/skeletons/buttons/google_MB48.tsx
+++ kaari-web/src/components/skeletons/buttons/google_MB48.tsx
@@ -1,10 +1,10 @@
 import { GoogleMB48 } from "../../styles/buttons/white_Google_MB48_style";
 
 
-export const GoogleButtonMB48 = ({ text, icon }: { text: String, icon?: String}) => {
+export const GoogleButtonMB48 = ({ text, icon }: { text: string, icon?: string}) => {
   return (
     <GoogleMB48>
-        {icon}
+        {icon && <img src={icon} alt="Google icon" />}
         {text}
     </GoogleMB48>
   )
